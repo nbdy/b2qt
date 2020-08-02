3ce07abb729dbd5a277ef29a18ff6ce3045502a0
smthnspcl <pascal@eberlein.io>
2020-08-02 16:24:00 +0200

    update

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ MACHINE="raspberrypi4"
 
 if [ $# -eq 1 ]; then QT_VERSION="$1"; elif [ $# -eq 2 ]; then QT_VERSION="$1"; MACHINE="$2"; fi
 
-docker build -t b2qt
+docker build -t b2qt .
 docker run -d -t b2qt -v "build/:/home/builder/build" "$QT_VERSION" "$MACHINE"
