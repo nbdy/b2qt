1460d1959da356463aae0a8560e28ac6190a5484
nbdy <pascal@eberlein.io>
2020-08-21 13:02:58 +0200

    update

--- b2qt.sh
+++ b2qt.sh
@@ -2,7 +2,7 @@
 
 QT_VERSION="5.15.0"
 MACHINE="raspberrypi4"
-CID=$(pwgen 20 1)
+CID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 
 if [ ! -d output/ ]; then mkdir output; fi
 
