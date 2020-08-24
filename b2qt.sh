5d19dc2a7cc4ff5b0ea2d2bd530d48a7d041d643
nbdy <pascal@eberlein.io>
2020-08-24 10:33:40 +0200

    update

--- b2qt.sh
+++ b2qt.sh
@@ -2,14 +2,13 @@
 
 QT_VERSION="5.15.0"
 MACHINE="raspberrypi4"
-CID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 
 if [ ! -d output/ ]; then mkdir output; fi
 
 if [ $# -eq 1 ]; then QT_VERSION="$1"; elif [ $# -eq 2 ]; then QT_VERSION="$1"; MACHINE="$2"; fi
 
 docker build -t b2qt .
-docker run -t b2qt --name "$CID" "$QT_VERSION" "$MACHINE"
+CID=$(docker run -d -t b2qt --name "$CID" "$QT_VERSION" "$MACHINE")
 
 SRC_IMAGE="/home/builder/$MACHINE/tmp/deploy/images/$MACHINE/b2qt-embedded-qt5-image-$MACHINE.img"
 
