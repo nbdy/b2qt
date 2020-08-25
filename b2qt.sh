96fdb9ce59700b53ec02a4f8a199fdf050173b58
nbdy <pascal@eberlein.io>
2020-08-25 04:07:49 +0200

    update

--- b2qt.sh
+++ b2qt.sh
@@ -8,8 +8,7 @@ if [ ! -d output/ ]; then mkdir output; fi
 if [ $# -eq 1 ]; then QT_VERSION="$1"; elif [ $# -eq 2 ]; then QT_VERSION="$1"; MACHINE="$2"; fi
 
 docker build -t b2qt .
-CID=$(docker run -d -t b2qt --name "$CID" "$QT_VERSION" "$MACHINE")
-
+docker run -t b2qt "$QT_VERSION" "$MACHINE"
+CID=$(docker container list -a -n 1 --format "{{.ID}}" -f "ancestor=b2qt")
 SRC_DIR="/home/builder/$MACHINE/tmp/deploy/images/$MACHINE/"
-
 docker cp "$CID:$SRC_DIR" ./output/"$MACHINE".img
